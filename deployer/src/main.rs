54cfaa18c80c7049686cb927b875aee70371648b
shaipe <shaipe@sina.com>
2019-07-23 18:07:22 +0800

    modify deployer

--- deployer/src/main.rs
+++ deployer/src/main.rs
@@ -5,8 +5,12 @@ use std::{fs};
 // use std::env;
 use std::path::{Path};
 // use clap::{Arg, App}; // value_t
-use std::time::SystemTime;
-use chrono::{DateTime, offset::Local as LocalTz}; //SubsecRound,
+// use std::time::SystemTime;
+// use chrono::{DateTime, offset::Local as LocalTz}; //SubsecRound,
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+// use std::path::Path;
 
 mod config;
 
@@ -39,9 +43,11 @@ fn main() {
     // let listen_port = value_t!(matches, "listen_port", u16).unwrap_or_else(|e| e.exit());
 
     let c = config::Config::new("config.json");
-    println!("{:?}", c);
-    let p = Path::new("./");
-    println!("{:?}", p.canonicalize().unwrap().as_path());
+
+    // println!("{:?}", c);
+    // let p = Path::new("./");
+    // println!("{:?}", p.canonicalize().unwrap().as_path());
+
     // println!("curdir: {}, curexe: {}", env::current_dir().unwrap().display(), env::current_exe().unwrap().display());
 
     // println!("cd to ../..\n");
@@ -58,32 +64,53 @@ fn main() {
     // }
     // let ss = str::
 
-    let x = String::from("s: &String");
-    let x = x.replace("s", "tos");
-    let start = SystemTime::now();
+    // let x = String::from("s: &String");
+    // let x = x.replace("s", "tos");
+    // let start = SystemTime::now();
 
   
 
-    println!("x:: {} , {:?}", x, start);
+    // println!("x:: {} , {:?}", x, start);
 
-    let p = Path::new("/users/shaipe/dist/");
-    walk_dir(&p);
+    let p = Path::new(&c.src);
+    println!("{:?}", p);
+
+    let write_path = format!("{}/files.csv", &c.dist);
+    println!("{}", write_path);
+    let mut file = match File::create(&write_path) {
+        Err(why) => panic!("couldn't create {}", why),
+        Ok(file) => file,
+    };
+
+    walk_dir(&p, &mut file);
 
 
 }
 
-
-fn walk_dir(dir: &Path) {
+/// 目录遍历
+fn walk_dir(dir: &Path, file: &mut File) {
     for entry in fs::read_dir(dir).unwrap() {
         let ent = entry.unwrap();
         let path = ent.path();
-        let data = ent.metadata().unwrap();
+        // let data = ent.metadata().unwrap();
         if path.is_dir(){
-            walk_dir(&path);
+            walk_dir(&path, file);
         }
         else{
 
-
+            let path_str = path.display();
+            let display = format!("{:?}\n", path_str);
+            // println!("{:?}", format!("{:?}", path_str).find("_"));
+            if display.find("_").is_none() {
+                // println!("{}", path.display());
+                match file.write_all(display.as_bytes()) {
+                    Err(why) => {
+                        panic!("couldn't write to : {}", why.description())
+                    },
+                    Ok(_) => {println!("successfully wrote to {}", display)},
+                };
+            }
+            
             // // Set `Last-Modified` and check `If-Modified-Since`.
             // if let Ok(modified) = metadata.modified() {
             //     let modified: DateTime<LocalTz> = modified.into();
@@ -103,19 +130,26 @@ fn walk_dir(dir: &Path) {
             //     res.header(header::ETAG, format!("W/\"{0:x}-{1:x}.{2:x}\"",
             //         metadata.len(), modified.timestamp(), modified.timestamp_subsec_nanos()).as_str());
             // }
-            let dt = DateTime::parse_from_str("2019-07-01 10:31:19 +08:00", "%Y-%m-%d %H:%M:%S %z");
+            
+            
+            // begin
+            // let dt = DateTime::parse_from_str("2019-07-01 10:31:19 +08:00", "%Y-%m-%d %H:%M:%S %z");
 
-            if let Ok(modified) = data.modified() {
-                let modified: DateTime<LocalTz> = modified.into();
-                // Some(dt) if modified.trunc_subsecs(0) >= dt.trunc_subsecs(0) => {
-                //     return "x"
-                // }
+            // if let Ok(modified) = data.modified() {
+            //     let modified: DateTime<LocalTz> = modified.into();
+            //     // Some(dt) if modified.trunc_subsecs(0) >= dt.trunc_subsecs(0) => {
+            //     //     return "x"
+            //     // }
 
-                println!("{:?}, dt : {:?}", modified, dt);
-            }
-            if let Ok(time) = data.modified() {
-                println!("{:?}", time);
-            }
+            //     println!("{:?}, dt : {:?}", modified, dt);
+            // }
+            // if let Ok(time) = data.modified() {
+            //     println!("{:?}", time);
+            // }
+            // end
+
+
+
             // if data.is_file() {
             //     if let Some(ex) = path.extension() {
             //         if ex == "js" && data.len() > 1024 {
