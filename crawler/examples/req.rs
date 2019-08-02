667a401d30377aa76c852402455ab7aa3e3dca29
shaipe <shaipe@sina.com>
2019-08-02 22:25:17 +0800

    modify req

--- crawler/examples/req.rs
+++ crawler/examples/req.rs
@@ -1,11 +1,24 @@
+
 extern crate reqwest;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+
+use std::env;
+use reqwest::Client;
 
 use reqwest::Error;
 use std::time::Duration;
 use reqwest::ClientBuilder;
 
-
 fn main() -> Result<(), Error> {
+    let _ = run1();
+    let _ = run();
+    Ok(())
+}
+
+fn run1() -> Result<(), Error> {
     let user = "shaipe";
     let request_url = format!("https://api.github.com/users/{}", user);
     println!("{}", request_url);
@@ -20,5 +33,46 @@ fn main() -> Result<(), Error> {
         println!("{} is not a user!", user);
     }
 
+    Ok(())
+}
+
+
+
+#[derive(Deserialize, Debug)]
+struct Gist {
+    id: String,
+    html_url: String,
+}
+
+fn run() -> Result<(), Error> {
+    let gh_user = "ss";
+    let gh_pass = "ps";
+
+    let gist_body = json!({
+        "description": "the description for this gist",
+        "public": true,
+        "files": {
+             "main.rs": {
+             "content": r#"fn main() { println!("hello world!");}"#
+            }
+        }});
+
+    let request_url = "https://api.github.com/gists";
+    let mut response = Client::new()
+        .post(request_url)
+        .basic_auth(gh_user.clone(), Some(gh_pass.clone()))
+        .json(&gist_body)
+        .send()?;
+
+    let gist: Gist = response.json()?;
+    println!("Created {:?}", gist);
+
+    let request_url = format!("{}/{}",request_url, gist.id);
+    let response = Client::new()
+        .delete(&request_url)
+        .basic_auth(gh_user, Some(gh_pass))
+        .send()?;
+
+    println!("Gist {} deleted! Status code: {}",gist.id, response.status());
     Ok(())
 }
