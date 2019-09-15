e3f3d30e2e736bf3d62ba241a54c0ba4d7118329
shaipe <shaipe@sina.com>
2019-09-15 17:53:08 +0800

    modify rust to sql tool

--- coder/examples/rust2sql.rs
+++ coder/examples/rust2sql.rs
@@ -3,34 +3,94 @@
  * create by shaipe
  * create time 20190901
  */
+use std::env;
 use lane::fs::{read_content, append_content};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
+/// 启动函数
 fn main() {
-
-    let dir_path = Path::new("/Users/shaipe/workspace/rust/sst/sst-stock/src/");
-
-    let files: Vec<&str> = vec![
-        "stock.rs",
-        "hist.rs",
-        "real.rs"
-    ];
-
-    for f in files {
-        let file_path = dir_path.join(f);
-        let file_str = format!("{}", file_path.display());
-        println!("{}", file_str);
-        let content = read_content(&file_str);
-        
-        let sql = get_create_sql(content);
-        append_content("/Users/shaipe/workspace/rust/sst/resources/init.sql", sql.as_bytes());
-        // println!("{}", sql);
+    // 获取输入参数
+    let args: Vec<String> = env::args().collect();
+    
+    let prefix = if args.len() > 1 {
+        // let mut t = String::from(&args[2]);
+        // t.push_str("_");
+        // t
+        &args[1]
     }
+    else{
+        ""
+    };
+
+    let dir_str = if args.len() > 2 {
+        &args[2]
+    }
+    else{
+        "./"
+    };
+
+    // 获取目录路径
+    let dir_path = Path::new(dir_str);
+    let file_path = dir_path.join("./tables.sql");
+    let file_path_str = format!("{}", file_path.display());
+
+    println!("开始对 {:?} 目录进行处理, 将把结果写入 {:?} 文件中.", dir_path.display(), file_path.display());
+
+    walk(PathBuf::from(dir_path), &file_path_str, prefix);
+
+    println!("代码生成已经完成");
+    // let files: Vec<&str> = vec![
+    //     "stock.rs",
+    //     "hist.rs",
+    //     "real.rs"
+    // ];
+
+    // for f in files {
+    //     let file_path = dir_path.join(f);
+    //     let file_str = format!("{}", file_path.display());
+    //     println!("{}", file_str);
+    //     let content = read_content(&file_str);
+        
+    //     let sql = get_create_sql(content);
+    //     append_content("/Users/shaipe/workspace/rust/sst/resources/init.sql", sql.as_bytes());
+    //     // println!("{}", sql);
+    // }
     
     
 }
 
-fn get_create_sql(content: String) -> String {
+/// 遍历整个目录对指定的rs文件进行处理
+fn walk(dir_path: PathBuf, file_path: &str, prefix: &str) {
+    if dir_path.exists() {
+        for entry in dir_path.read_dir().expect("指定的路径 {} 不存在.") {
+            let p = entry.unwrap().path();
+            // println!("{:?}", p);
+            if p.is_file() {
+                let ext = p.extension();
+                // 判断只处理后缀为rs的文件
+                if !ext.is_none() && ext.unwrap() == "rs" {
+                    // 获取路径的字符
+                    let p_str = format!("{}", p.display());
+                    // 判断只处理src目录下的文件
+                    if p_str.contains(r"/src/"){
+                        let content = read_content(&p_str);
+                        let sql = get_create_sql(content, prefix);
+                        append_content(file_path, sql.as_bytes());
+                    }
+                }
+            }
+            else{
+                walk(p, file_path, prefix);
+            }
+        }
+    }
+    else{
+        println!("指定的路径 {} 不存在.", dir_path.display());
+    }
+}
+
+/// 根据文件内容获取创建表的sql语句
+fn get_create_sql(content: String, prefix: &str) -> String {
 
     let lines: Vec<&str> = content.split("\n").collect();
 
@@ -45,7 +105,9 @@ fn get_create_sql(content: String) -> String {
             if line.contains("struct ") {
                 structed = true;
                 let y = line.replace("pub struct ", "").replace("{", "");
-                let yx = y.trim().to_lowercase();
+                let mut yx = String::new();
+                yx.push_str(prefix);
+                yx.push_str(&y.trim().to_lowercase());
                 // println!("{:?}",yx);
                 res.push(format!("-- table {} ", yx));
                 res.push(format!("CREATE TABLE `{}` (", yx));
@@ -61,6 +123,10 @@ fn get_create_sql(content: String) -> String {
                     else if line.contains(":"){
                         let l = line.replace("pub ", "");
                         let f: Vec<&str> = l.split(":").collect();
+                        // 如果有字段的名字为id跳过,已经为表添加了默认的id字段 
+                        if f[0].trim().to_lowercase().as_str() == "id" {
+                            continue;
+                        }
                         let x: Vec<&str> = f[1].split(",").collect();
 
                         let comment = if x.len() > 1 {
@@ -81,13 +147,15 @@ fn get_create_sql(content: String) -> String {
     res.join("\n")
 }
 
-
+/// 荛取数据类型
 fn get_data_type(type_str: &str) -> String{
     // println!("{}", type_str);
     match type_str.to_lowercase().trim() {
+        "i8" => "SMALLINT".to_owned(),
         "i64" | "i32" => "INT(11)".to_owned(),
         "f64" | "f32" => "FLOAT(12,6)".to_owned(),
-        "datetime<local>" | "datetime<utc>" => "DateTime".to_owned(),
+        "datetime<local>" | "datetime<utc>" | "naivedatetime" => "DATETIME".to_owned(),
+        "naivedate" => "DATE".to_owned(),
         _ => "VARCHAR(50)".to_owned()
     }
 }
