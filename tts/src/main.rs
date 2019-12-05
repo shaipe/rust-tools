5b1dd3422a4ef7a367127cfe81096de793ef6c99
shaipe <shaipe@sina.com>
2019-12-05 08:41:33 +0800

    modify tts

--- tts/src/main.rs
+++ tts/src/main.rs
@@ -6,13 +6,15 @@ use tokio::timer::Interval;
 use std::time::{Duration, Instant};
 mod config;
 use config::Config;
-
+// use std::ops::Try::from_error;
+use std::collections::HashMap;
 
 fn main() {
     // work(100);
-    let cnf = Config::new("");
-    println!("{:?}", cnf);
+    // let cnf = Config::new("");
+    // println!("{:?}", cnf);
     // work(1000);
+    post();
 }
 
 fn work(interval: u64){
@@ -25,4 +27,17 @@ fn work(interval: u64){
         .map_err(|e| panic!("interval errored; err={:?}", e));
 
     tokio::run(task);
+}
+
+fn post() {
+    let mut map = HashMap::new();
+    map.insert("databaseId", "22");
+    map.insert("appid", "tts_app");
+    map.insert("method", "sch.overall.exec");
+
+    let client = reqwest::Client::new();
+    let res = client.post("http://127.0.0.1:8000/das")
+        .json(&map)
+        .send();
+    println!("{:?}", res);
 }
