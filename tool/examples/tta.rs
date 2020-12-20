600dc8a5cd097ac22ad17350d4d6810e89fb6e99
shaipe <shaipe@live.com>
2020-12-20 10:04:27 +0800

    add deref test

---
+++ tool/examples/tta.rs
@@ -0,0 +1,41 @@
+#[derive(Debug, Clone)]
+struct Book {
+    name: String
+}
+
+impl Book {
+     fn show_book_name(&self) {
+         // .... 
+         println!("name is: {}", self.name);
+     }
+}
+
+use std::ops::{Deref};
+
+#[derive(Debug, Clone)]
+struct MyBook {
+    p: Book,
+    author: String,
+}
+
+impl MyBook {
+    fn new() -> MyBook {
+        MyBook {
+            p: Book{name: "知乎大全".to_string()},
+            author: "我是谁".to_string()
+       }
+     }
+}
+
+impl Deref for MyBook {
+     type Target = Book;
+
+     fn deref<'a>(&'a self) -> &'a Book {
+          &self.p
+}
+}
+fn main() {
+    let mut mybook = MyBook::new();
+    println!("{:?}", mybook.name);
+    mybook.show_book_name();
+}
